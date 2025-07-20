9cdd017c557b0b8c37f5787cc9b92780875ad54f
Abuzar Alvi <143905822+abuzar-alvi@users.noreply.github.com>
2025-07-20 05:47:59 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <title>Webia Solutionz</title>
+        <title>Webia Solutionz – Modern Digital Agency Website</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
